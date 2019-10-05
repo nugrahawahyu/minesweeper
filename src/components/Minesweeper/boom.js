ffec275bfd14dfb07026b70daeca143feac9e631
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-10-05 14:39:10 +0700

    simple boom animation

--- src/components/Minesweeper/boom.js
+++ src/components/Minesweeper/boom.js
@@ -1,9 +1,24 @@
+function animateHelper (index, length, mineNodes, duration, beforeNextCallback, resolve) {
+    if (index >= length) {
+        resolve()
+        return
+    }
+
+    const mineNode = mineNodes[index]
+    const wait = duration / (mineNodes.length)
+
+    setTimeout(() => {
+        mineNode.mark()
+        beforeNextCallback()
+        animateHelper(index+1, length, mineNodes, duration, beforeNextCallback, resolve)
+    }, wait)
+}
+
 export class Boom {
-    static animate () {
-        
+    static animate (mineNodes, duration, beforeNextCallback = () => {}) {
+        return new Promise((resolve) => {
+            animateHelper(0, mineNodes.length, mineNodes, duration, beforeNextCallback, resolve)
+        })
     }
 }
 
-Boom.items = []
-Boom.duration = 1000
-Boom.callback = () => {}
